bdac1ee7992e6433f22b25fc3d8e0024a83c533e
Scream <scream870102@gmail.com>
2020-11-19 19:43:32 +0900

    17 ~fin

---
+++ euler016.rb
@@ -0,0 +1,12 @@
+# Power digit sum
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 2^1000?
+
+def euler016
+    sum=0
+    (2**1000).to_s.split("").each{|i|sum+=i.to_i}
+    return sum
+end
+
+puts euler016
---
+++ euler018.rb
@@ -0,0 +1,72 @@
+# By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+# 3
+# 7 4
+# 2 4 6
+# 8 5 9 3
+
+# That is, 3 + 7 + 4 + 9 = 23.
+
+# Find the maximum total from top to bottom of the triangle below:
+
+# 75
+# 95 64
+# 17 47 82
+# 18 35 87 10
+# 20 04 82 47 65
+# 19 01 23 75 03 34
+# 88 02 77 73 07 63 67
+# 99 65 04 28 06 16 70 92
+# 41 41 26 56 83 40 80 70 33
+# 41 48 72 33 47 32 37 16 94 29
+# 53 71 44 65 25 43 91 52 97 51 14
+# 70 11 33 28 77 73 17 78 39 68 17 57
+# 91 71 52 38 17 14 91 43 58 50 27 29 48
+# 63 66 04 68 89 53 67 30 73 16 69 87 40 31
+# 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+
+# NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
+def euler018
+s=
+"75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+array=s.split("\n").map{|r|r.split(" ")}
+for (int h = triangleArray.length - 1; h > 0; h--) {
+    for (int w = 0; w < triangleArray[h - 1].length; w++) {
+        triangleArray[h - 1][w] = triangleArray[h - 1][w]
+                + Integer.max(triangleArray[h][w],
+                        triangleArray[h][w + 1]);
+    }
+}
+ 
+System.out.println(triangleArray[0][0]);
+# sum = array[0][0].to_i
+# c=0
+# for i in (0...14)
+#     max=0
+#     if array[i+1][c].to_i>array[i+1][c+1].to_i
+#         max = array[i+1][c].to_i
+#     else
+#         max = array[i+1][c+1].to_i
+#         c+=1
+#     end
+#     puts max
+#     sum+=max
+# end
+# return sum
+end
+
+puts euler018
